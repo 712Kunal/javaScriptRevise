dc9d48eae92941061100de90d863eecfdf0c6b59
712Kunal <jadhavkun0@gmail.com>
2026-08-03 19:47:05 +0530

    added CRUD in objects

--- lec6/Objects.js
+++ lec6/Objects.js
@@ -1,6 +1,6 @@
 // WAYS OF CREATING OBJECTS =>
 
-// 1. USING LITERALS
+// 1. USING LITERALS (OBJECT LITERAL)
 let obj = {
   key: "value",
 };
@@ -15,3 +15,17 @@ let laptop = {
 console.log(laptop); // { brand: 'hp', price: 50000 }
 console.log(typeof laptop); // object
 
+console.log(laptop.brand); // hp
+
+// adding new property
+laptop.color = "red";
+console.log(laptop); // { brand: 'hp', price: 50000, color: 'red' }
+
+// updating existing property
+laptop.price = 100000;
+console.log(laptop); // { brand: 'hp', price: 100000, color: 'red' }
+
+// deleting existing property
+delete laptop.color;
+console.log(laptop); // { brand: 'hp', price: 100000 }
+console.log(laptop.color); // undefined
