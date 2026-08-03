1e476775aad6acf3a2cf21966ff8b6dc51ceddeb
712Kunal <jadhavkun0@gmail.com>
2026-08-03 19:40:06 +0530

    added the objects file

---
+++ lec6/Objects.js
@@ -0,0 +1,17 @@
+// WAYS OF CREATING OBJECTS =>
+
+// 1. USING LITERALS
+let obj = {
+  key: "value",
+};
+
+console.log(obj); // { key: 'value' }
+
+let laptop = {
+  brand: "hp",
+  price: 50000,
+};
+
+console.log(laptop); // { brand: 'hp', price: 50000 }
+console.log(typeof laptop); // object
+
