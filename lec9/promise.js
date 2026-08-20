8d7a521f8118c4317146f66991fe4b77b4574b0d
712Kunal <jadhavkun0@gmail.com>
2026-08-20 18:32:29 +0530

    aded the promises code

--- lec9/promise.js
+++ lec9/promise.js
@@ -1,3 +1,4 @@
+// PROMISE IS USED TO HANDLE ASYNC OPERATIONS
 let p = new Promise((resolve, reject) => {
   let x = true;
 
@@ -10,6 +11,28 @@ let p = new Promise((resolve, reject) => {
 
 console.log(p);
 
+// if promise get resolved, "then" block will be executed
 p.then((res) => {
   console.log(res);
 });
+
+// if promise get rejected, "catch" block will be executed
+p.catch((err) => {
+  console.log(err);
+});
+
+// "finally" block will be executed whether promise get resolved or rejectedz
+p.finally(() => {
+  console.log(`Promise completed`);
+});
+
+// All in single line
+p.then((res) => {
+  console.log(res);
+})
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log(`Promise completed`);
+  });
