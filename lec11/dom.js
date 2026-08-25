5acf2eb72f511cceef5b1cf0a1c38ba0aaf2a5a9
712Kunal <jadhavkun0@gmail.com>
2026-08-25 19:33:15 +0530

    added the dom code

--- lec11/dom.js
+++ lec11/dom.js
@@ -11,3 +11,25 @@ let changeText = () => {
   x.style.fontSize = "5em";
   x.innerText = "Hello React js";
 };
+
+let y = document.getElementsByClassName("text");
+console.log(y);
+
+let z = document.getElementsByTagName("h1");
+console.log(z);
+
+let a = document.querySelector("h1"); // we can pass anything but it will target only first one
+console.log(a);
+
+let b = document.querySelector(".text");
+console.log(b);
+
+let c = document.querySelector("#react");
+console.log(c);
+
+let d = document.querySelectorAll("h1"); // it will target all elements
+console.log(d);
+
+let e = document.querySelectorAll("#react");
+console.log(e);
+
