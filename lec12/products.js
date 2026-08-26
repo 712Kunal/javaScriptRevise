302b1584323bf6f7e99051a4f3880fdcff96d67e
712Kunal <jadhavkun0@gmail.com>
2026-08-26 20:22:49 +0530

    added the styling and some functionality

--- lec12/products.js
+++ lec12/products.js
@@ -2,7 +2,33 @@ let allProducts = [];
 let container = document.getElementById("products-container");
 
 let printProducts = (products) => {
-  products.map(() => {});
+  container.innerHTML = products
+    .map(
+      (product) => `
+    <div id="product-card">
+        <img src="${product.thumbnail}" alt="${product.title}" id="product-img">
+        <h1>${product.title}</h1>
+        <h2>Price: ${Math.round(product.price * 95)}</h2>
+        <p>Ratings: ${product.rating}</p>
+        <button>Add to cart</button>
+    </div>
+  `,
+    )
+    .join("");
+};
+
+let below500 = () => {
+  let filteredProducts = allProducts.filter(
+    (product) => Math.round(product.price * 95) < 500,
+  );
+  printProducts(filteredProducts);
+};
+
+let above500 = () => {
+  let filteredProducts = allProducts.filter(
+    (product) => Math.round(product.price * 95) > 500,
+  );
+  printProducts(filteredProducts);
 };
 
 let fetchingProducts = async () => {
