bb54e4719330efe54835ab570696d7a60e90014a
712Kunal <jadhavkun0@gmail.com>
2026-09-01 19:18:06 +0530

    added the event loop code

--- lec14/TimerFunc.js
+++ lec14/TimerFunc.js
@@ -34,3 +34,64 @@ let start = () => {
 let stop = () => {
   clearInterval(b);
 };
+
+let mini = document.getElementById("minutes");
+let sec = document.getElementById("seconds");
+
+let c;
+let secCnt = 0;
+let minCnt = 0;
+
+let startCntr = () => {
+  c = setInterval(() => {
+    secCnt++;
+    if (secCnt == 60) {
+      minCnt++;
+      secCnt = 0;
+      mini.innerText = minCnt;
+    }
+    sec.innerText = secCnt;
+  }, 1000);
+};
+
+let stopCntr = () => {
+  clearInterval(c);
+};
+
+function first() {
+  second();
+  console.log("First function executed");
+}
+
+function second() {
+  third();
+  console.log("Second function executed");
+}
+
+function third() {
+  console.log("Third function executed");
+}
+
+first();
+
+console.log("Start");
+setTimeout(() => {
+  console.log("Hello");
+}, 1000);
+setTimeout(() => {
+  console.log("World");
+}, 0);
+
+function func() {
+  console.log("Function called");
+}
+
+func();
+console.log("end");
+
+// BrowserApi will store all the asynchronous operations(api calls,setTimeout,setInterval,fetch)
+// setTimeout and setInterval are stored in the macro task queue
+// api calls and fetch are stored in the micro task queue
+// synchronous operations will stored in the callstack
+// Event loop checks weather the callstack is empty or not, if empty it goes to the queues
+// Event loop always gives first preference to the micro task queue and then macro task queue
